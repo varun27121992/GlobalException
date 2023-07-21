26540b41a60ac23194bfdc8ef4e3daf43a670a6e
varun27121992 <varun27121992@gmail.com>
2023-07-21 15:29:06 +0530

    updated

--- ExceptionCustom.java
+++ ExceptionCustom.java
@@ -5,16 +5,17 @@ public class ExceptionCustom {
 	// TODO Auto-generated method stub
 
 	public void passwordCheck(String a) throws PasswordException {
-try {
-	if (a.length() >= 8) {
-		System.out.println("password is vaild");
-		} else {
-		throw new PasswordException("password is invalid");
+		try {
+			if (a.length() >= 8) {
+				System.out.println("password is vaild");
+			} else {
+				throw new PasswordException("password is invalid");
+			}
+		} catch (PasswordException r) {
+
+			r.printStacktrace();
+			System.out.println(r);
+			System.out.println("GIT");
 		}
-}
-catch (PasswordException r){
-	System.out.println(r);
-	r.printStacktrace();
-}
 	}
 }
